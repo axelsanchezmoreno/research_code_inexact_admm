dd0e14672449a6cc3d64a4bc4cd8ed61b569c9dd
axelsanchezmoreno <a_s1350@txstate.edu>
2026-09-25 21:56:45 -0500

    Creating baseline code, making sure there's no errors moving forward.

--- iadmm_xie/main.py
+++ iadmm_xie/main.py
@@ -1,37 +1,51 @@
 import numpy as np
 
-from cg_method import cg_class
-
 ## Comments:
 ## This code implements the inexact ADMM with relative error 
 ## criterion algorithm. The main loop is performing the minimization problem. 
 ## Author(s): Jiaxin Xie
 
-def main():
-    m, n, s = 2048, 256, 512
-    A, D = np.random.randn(m, n), np.random.randn(s, n)
-    c = np.random.randn(m)
+# from algorithms import iadmm_algorithms
+# from data_generation import gen_data
 
-    # Defining the unknown variables
-    x_p, y_p, l_d = np.zeros(n), np.random.randn(s), np.random.randn(s)
+def main():
+    # Dimensions of the problems, sparsity of vector definition
+    m, n = 2048, 256
+    s = np.round(0.1 * m)
+
+    mat_def = 1             ## Specifying how the D matrix is defined
+    method_def = 'CG'       ## Specify how we solve the x-subproblem
+
 
     # Defining the constants, mu, beta, and sigma
     mu, beta, sigma = 0.01, 25, 0.5
 
-    cg_dict = {
-        "A" : A,
-        "D" : D,
-        "y" : y_p,
-        "l" : l_d,
-        "c" : c,
-        "mu" : mu,
-        "beta" : beta, 
-        "sigma" : sigma
-    }
+    print(f"Problem: m, n, s = {m}, {n}, {s}\n")
 
-    # Executing the CG method
-    conjugate_class = cg_class(**cg_dict)
-    x_p = conjugate_class.cg_method(x_p)
+    for trial in range(1, 11):
+        # A, D, x_sol, c = gen_data(m, n, s, mat_def)
+        # iadmm_algorithms(A, D, c, m, n, s, method_def)
+        pass
+
+    # A, D = np.random.randn(m, n), np.random.randn(s, n)
+    # c = np.random.randn(m)
+    # # Defining the unknown variables
+    # x_p, y_p, l_d = np.zeros(n), np.random.randn(s), np.random.randn(s)
+
+    # cg_dict = {
+    #     "A" : A,
+    #     "D" : D,
+    #     "y" : y_p,
+    #     "l" : l_d,
+    #     "c" : c,
+    #     "mu" : mu,
+    #     "beta" : beta, 
+    #     "sigma" : sigma
+    # }
+
+    # # Executing the CG method
+    # conjugate_class = cg_class(**cg_dict)
+    # x_p = conjugate_class.cg_method(x_p)
 
 if __name__ == '__main__':
 	main()
